0f0901ff465be91df26d623e0080e4129bba2fbd
billelX20X <164456584+billelX20X@users.noreply.github.com>
2026-01-09 15:27:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,17 @@
-
 from flask import Flask, request, jsonify
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello from Flask!'
-
-
-import time
+    return "Hello from Flask!"
 
 @app.route("/test", methods=["POST"])
 def test():
     data = request.json
-
     time.sleep(1)
-
     return jsonify({
         "status": "success",
         "user_id": data.get("user_id")
     })
-
-if __name__ =="__main__":
-    app.run()
-
-
-
